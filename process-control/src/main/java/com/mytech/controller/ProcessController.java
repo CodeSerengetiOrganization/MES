61732986a6082bf5cc82fce893eec32f75caadd8
Qiang <birareos.wang@gmail.com>
2022-11-05 01:59:48 -0400

    1. added 3 DTOs for process control:StateDTO, StateInputDTO, stateOutputDTO; 2. added procdessProductState() method in ProcessController

--- process-control/src/main/java/com/mytech/controller/ProcessController.java
+++ process-control/src/main/java/com/mytech/controller/ProcessController.java
@@ -1,11 +1,18 @@
 package com.mytech.controller;
 
 import com.google.common.base.Preconditions;
+import com.mytech.domain.ProcessEventEnum;
+import com.mytech.domain.ProcessStateEnum;
+import com.mytech.dto.StateInputDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 import com.mytech.service.ManufacturingProcessService;
 
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 /**
@@ -44,6 +51,36 @@ public class ProcessController {
 /*        manufacturingService.updateManufacturingStatus(msg);
         Message<ProcessEventEnum> message = MessageBuilder.withPayload(ProcessEventEnum.valueOf(msg.get("event"))).setHeader("barcode", msg.get("barcode")).build();
         manufacturingService.updateManufacturingStatusMessage(message);*/
-    }//updateManufacturingStatus
+    }//updateManufacturingStatusj
 
+    @PutMapping("/production/v1/productstate")
+    public void processProductState(@RequestBody StateInputDTO inputDTO){
+        ProcessEventEnum event = inputDTO.getEvent();
+        //build a Message
+        Message<ProcessEventEnum> processStateEnumMessage = MessageBuilder
+                .withPayload(event)
+                .setHeader("stationName",inputDTO.getStationName())
+                .setHeader("barcode",inputDTO.getBarcode())
+                .build();
+        //send to service to process, which will use state machine
+        MessageHeaders headers = processStateEnumMessage.getHeaders();
+        String barcodeStr= (String) headers.get("barcode");
+        String stationNameStr = headers.get("stationName", String.class);
+        String eventStr=processStateEnumMessage.getPayload().toString();
+//        String barcodeStr=processStateEnumMessage.getHeaders();
+        boolean result = service.processManufacturingResult(barcodeStr, eventStr);
+        System.out.println("event Enum:"+event);
+        System.out.println("inputDTO:"+inputDTO.toString());
+    }
+
+    /**
+     * This is a simple test method to see if the controller could parse a ProcessStateEnum object from JSON string
+     * @param inputDTO
+     */
+    @PutMapping("/production/v1/getInputDtoWithEnum")
+    public void getInputDtoWithEnum(@RequestBody StateInputDTO inputDTO){
+        ProcessEventEnum event = inputDTO.getEvent();
+        System.out.println("event Enum:"+event);
+        System.out.println("inputDTO:"+inputDTO.toString());
+    }
 }//class
